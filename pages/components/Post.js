ea7784e740c0db4ca15cc3ed40ec40a50ca0b057
Brian Jenney <brianjenney83@gmail.com>
2023-09-04 11:37:43 -0700

    add hide/show and upvotes

--- pages/components/Post.js
+++ pages/components/Post.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Post = ({ post }) => (
-	<li>
-		<span>
-			{post.user} - {post.text}
-		</span>
-	</li>
-);
+const Post = ({ post, upVote }) => {
+	const [isHidden, setIsHidden] = useState(false);
+
+	const toggleShow = () => {
+		setIsHidden(!isHidden);
+	};
+
+	return (
+		<li>
+			{isHidden ? null : (
+				<span>
+					{post.user} - {post.text}
+				</span>
+			)}
+			<div className='pull-right'>
+				<button
+					className='btn btn-primary btn-sm'
+					onClick={() => upVote(post)}
+				>
+					Upvote
+				</button>
+				{isHidden ? (
+					<button
+						onClick={toggleShow}
+						className='btn btn-success btn-sm'
+					>
+						Show
+					</button>
+				) : (
+					<button
+						onClick={toggleShow}
+						className='btn btn-danger btn-sm'
+					>
+						Hide
+					</button>
+				)}
+			</div>
+			<span>{post.votes}</span>
+		</li>
+	);
+};
 
 export default Post;
--- pages/components/PostList.js
+++ pages/components/PostList.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import Post from './Post';
 
-const PostsList = (props) => {
-	const postItems = props.posts.map((post, index) => (
-		<Post key={index} post={post} />
+const PostsList = ({ posts, upVote }) => {
+	const postItems = posts.map((post, index) => (
+		<Post upVote={upVote} key={index} post={post} />
 	));
 
 	return <ul className='col-md-4 list-group'>{postItems}</ul>;
